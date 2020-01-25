4d4daf0d03bacd79efb7713c4d53cf9bcc522d06
Marcel <marcelix161@gmail.com>
2020-01-25 20:11:57 +0100

    Created db model Posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(blank=True, max_length=100, null=True)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title

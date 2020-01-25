ff05ec8f5cd5bcc42427a79198295695e075a804
Marcel <marcelix161@gmail.com>
2020-01-25 21:37:47 +0100

    Name of the db model changed to Post

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Posts(models.Model):
+class Post(models.Model):
     title = models.CharField(blank=True, max_length=100, null=True)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)

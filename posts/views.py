3007a9b5d8d8c5eba5d04a63929f1d17c5103988
Marcel <marcelix161@gmail.com>
2020-01-26 13:52:00 +0100

    Added not generic view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
+from django.core.paginator import Paginator  # for vanilla View
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import ListView
 from .models import Post
 
@@ -6,5 +8,20 @@ from .models import Post
 class PostsListView(ListView):
     context_object_name = "posts"
     model = Post
+    ordering = ["-created_at"]
     paginate_by = 3
-    template_name = 'lists_of_posts.html'
+    template_name = "lists_of_posts.html"
+
+
+class PostsView(View):
+    def get(self, request):
+        posts = Post.objects.all().order_by("-created_at")
+        paginator = Paginator(posts, 3)
+        page = request.GET.get("page")
+        posts = paginator.get_page(page)
+
+        ctx = {
+            "posts": posts,
+        }
+
+        return render(request, "posts_view.html", ctx)
